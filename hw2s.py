63b37e0a17b17d6be447c90055e4b983a176c716
raquelj6437 <raquelj6437@hstat.org>
2018-07-03 10:30:43 -0400

    allow user to input multiple list

--- hw2s.py
+++ hw2s.py
@@ -1,35 +1,21 @@
 num = input('enter amount of matrices you would like to add, subtract, or multiply ')
 matricesList = ['d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-a=[]
-b=[]
-c=[]
 
-def matrices():
-    a = input('enter first matrix ')
-    a = a.split(',')
-    b = input('enter second matrix ')
-    b = b.split(',')
-    c = input('enter third matricx ')
-    c = c.split(',')
+for x in range(0,int(num)):
+  a = input('enter first matrix ')
+  a = a.split(',')
+  b = input('enter second matrix ')
+  b = b.split(',')
+  c = input('enter third matricx ')
+  c = c.split(',')
 
-for x in range(1,int(num)+1):
-  matrices()
-  for x in range(0, int(num)):
-      if len(a) == len(b) and len(b) == len(c):
-          matricesList[x]=[]
-          matricesList[x].append(a)
-          matricesList[x].append(b)
-          matricesList[x].append(c)
+  if len(a) == len(b) and len(b) == len(c):
+      vars()[matricesList[x]] = []
 
-          print(d)
-          print(e)
-          print(f)
-
-#
-#     for row in d:
-#         for elem in row:
-#             print(elem, end=' ')
-#         print()
-# else:
-#     print('the quantity of numbers in the matrix should be consistent with other matrices')
-#     matrices()
+      vars()[matricesList[x]].append(a)
+      vars()[matricesList[x]].append(b)
+      vars()[matricesList[x]].append(c)
+      print(vars()[matricesList[x]])
+  else:
+      print('the quantity of numbers in the matrix should be consistent with other matrices')
+      exit()
